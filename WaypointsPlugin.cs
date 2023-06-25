d68230acef7806fed25a8320417a8c76198e3f64
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-06-25 11:58:48 -0700

    - Build for 3.5.8, Update version

--- WaypointsPlugin.cs
+++ WaypointsPlugin.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 
 namespace DrakiaXYZ.Waypoints
 {
-    [BepInPlugin("xyz.drakia.waypoints", "DrakiaXYZ-Waypoints", "1.1.1")]
+    [BepInPlugin("xyz.drakia.waypoints", "DrakiaXYZ-Waypoints", "1.1.2")]
     public class WaypointsPlugin : BaseUnityPlugin
     {
         public static string PluginFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
