e5e41a4d1228fea09079db84e4b9a35195056d20
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-10-01 13:19:45 -0700

    - Update for EFT 26282

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: TarkovVersion(25800)]
+[assembly: TarkovVersion(26282)]
