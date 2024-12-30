cfa36b0f7d6417b82c06f4f140b37fd1ae1efdb3
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2024-12-30 14:46:05 -0800

    - Update version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DrakiaXYZ-Waypoints")]
-[assembly: AssemblyCopyright("Copyright ©  2023 DrakiaXYZ")]
+[assembly: AssemblyCopyright("Copyright ©  2024 DrakiaXYZ")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
 [assembly: TarkovVersion(33420)]
